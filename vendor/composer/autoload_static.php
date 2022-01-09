9082257630e44c994bccbf060690261837e1c9fe
Thomas Skerbis <skerbis@klxm.de>
2022-01-09 17:35:16 +0100

    1.3.0
    Vendor-Updates

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit5471e79cdf6c234cef2a81460bd6959f
 {
     public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Zebra_Image' => __DIR__ . '/..' . '/stefangabos/zebra_image/Zebra_Image.php',
     );
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'redaxo-addon',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'afa867a300dbb477825aefad5c399b4ab0e5daa5',
+        'name' => 'for/cropper',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'for/cropper' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'redaxo-addon',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'afa867a300dbb477825aefad5c399b4ab0e5daa5',
+            'dev_requirement' => false,
+        ),
+        'stefangabos/zebra_image' => array(
+            'pretty_version' => '2.6.0',
+            'version' => '2.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stefangabos/zebra_image',
+            'aliases' => array(),
+            'reference' => 'f23bafa20be9e930b47c6fd62a52914dd9cc1444',
+            'dev_requirement' => false,
+        ),
+    ),
+);
