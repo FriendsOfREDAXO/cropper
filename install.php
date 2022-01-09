5194d9ee13e4c3b3e129531cb8212c0742074fac
Thomas Skerbis <skerbis@klxm.de>
2022-01-09 17:50:04 +0100

    revert to 1.2

---
+++ install.php
@@ -0,0 +1,4 @@
+<?php
+
+/** @var rex_addon $this */
+
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
     'Zebra_Image' => $vendorDir . '/stefangabos/zebra_image/Zebra_Image.php',
 );
