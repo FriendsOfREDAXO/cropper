98cf26dacce8b039eaae6806e30c81ffcd8c7843
Thomas Skerbis <thomas.skerbis@mac.com>
2024-07-29 17:57:37 +0200

    Update install.php

--- install.php
+++ install.php
@@ -2,7 +2,6 @@
 
 $addon = rex_addon::get('cropper');
 
-rex_logger::factory()->log('notice', 'test', [], __FILE__, __LINE__);
 if (!$addon->hasConfig()) {
     $addon->setConfig('aspect_ratios', '16:9
 4:3
