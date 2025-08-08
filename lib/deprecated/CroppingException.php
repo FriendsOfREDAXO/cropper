b2a9275bd441bb3bdfb638e3e5b727e2b73e627b
christophboecker <christoph_boecker@t-online.de>
2025-08-08 16:41:41 +0200

    Alten Namespace in den deprecated-Klassen berücksichtigen

--- lib/deprecated/CroppingException.php
+++ lib/deprecated/CroppingException.php
@@ -1,6 +1,8 @@
 <?php
 
+namespace Cropper;
+
 /**
  * @deprecated Use FriendsOfRedaxo\Cropper\Cropper\CroppingException instead
  */
-class CroppingException extends FriendsOfRedaxo\Cropper\Cropper\CroppingException {}
+class CroppingException extends \FriendsOfRedaxo\Cropper\Cropper\CroppingException {}
